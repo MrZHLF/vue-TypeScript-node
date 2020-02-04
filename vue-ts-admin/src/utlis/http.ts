2daf05d617cdcf393917c35a66520793a3e030ad
MrzHLF <480970024@qq.com>
2020-02-04 14:22:15 +0800

    我的中心

--- vue-ts-admin/src/utlis/http.ts
+++ vue-ts-admin/src/utlis/http.ts
@@ -1,6 +1,6 @@
-import axios,{AxiosRequestConfig,AxiosResponse} from 'axios';
-import {Message} from 'element-ui';
-import router from '@/router';
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
+import { Message } from 'element-ui'
+import router from '@/router'
 /**
  * http请求工具类
  *
@@ -11,73 +11,80 @@ import router from '@/router';
 
 //  创建axios实列
 const service = axios.create({
-  timeout:1000
+  timeout: 1000
 })
 
 // 请求拦截
-service.interceptors.request.use((config:AxiosRequestConfig)=>{
-  if(localStorage.tsToken){
-    config.headers.Authorization = localStorage.tsToken;
+service.interceptors.request.use(
+  (config: AxiosRequestConfig) => {
+    if (localStorage.tsToken) {
+      config.headers.Authorization = localStorage.tsToken
+    }
+    return config
+  },
+  (err: any) => {
+    Promise.reject(err)
   }
-  return config;
-},(err:any)=>{
-  Promise.reject(err)
-});
+)
 
 // 响应拦截
-service.interceptors.response.use((response:AxiosResponse) =>{
-  return response;
-},(err:any) =>{
-  let errMsg = '';
+service.interceptors.response.use(
+  (response: AxiosResponse) => {
+    return response
+  },
+  (err: any) => {
+    console.log(err, 'err')
+    let errMsg = ''
     if (err && err.response.status) {
       switch (err.response.status) {
         case 401:
-          errMsg = '登录状态失效，请重新登录';
-          localStorage.removeItem('tsToken');
-          router.push('/login');
-          break;
+          errMsg = '登录状态失效，请重新登录'
+          localStorage.removeItem('tsToken')
+          router.push('/login')
+          break
         case 403:
-          errMsg = '拒绝访问';
-          break;
+          errMsg = '拒绝访问'
+          break
 
         case 408:
-          errMsg = '请求超时';
-          break;
+          errMsg = '请求超时'
+          break
 
         case 500:
-          errMsg = '服务器内部错误';
-          break;
+          errMsg = '服务器内部错误'
+          break
 
         case 501:
-          errMsg = '服务未实现';
-          break;
+          errMsg = '服务未实现'
+          break
 
         case 502:
-          errMsg = '网关错误';
-          break;
+          errMsg = '网关错误'
+          break
 
         case 503:
-          errMsg = '服务不可用';
-          break;
+          errMsg = '服务不可用'
+          break
 
         case 504:
-          errMsg = '网关超时';
-          break;
+          errMsg = '网关超时'
+          break
 
         case 505:
-          errMsg = 'HTTP版本不受支持';
-          break;
+          errMsg = 'HTTP版本不受支持'
+          break
 
         default:
-          errMsg = err.response.data.msg;
-          break;
+          errMsg = err.response.data.msg
+          break
       }
     } else {
-      errMsg = err;
+      errMsg = err
     }
 
-    Message.error(errMsg);
-    return Promise.reject(errMsg);
-});
+    Message.error(errMsg)
+    return Promise.reject(errMsg)
+  }
+)
 
-export default service;
+export default service
--- vue-ts-admin/src/views/UserManage/AccountData.vue
+++ vue-ts-admin/src/views/UserManage/AccountData.vue
@@ -1,14 +1,143 @@
 <template>
-  <div class="home">
-    AccountData
+  <div class="account-data">
+    <div class="add-box">
+      <el-button @click="addAccount" type="primary">新增账户</el-button>
+    </div>
+    <el-table :data="tableData"
+              border
+              style="width:100%">
+      <el-table-column label="角色"
+                       width="180px">
+        <template slot-scope="scope">
+          <el-select @change="selectChange(scope.row)"
+            v-if="scope.row.edit"
+            v-model="scope.row.role">
+            <el-option
+              v-for="option in options"
+              :label="option.role"
+              :value="option.role"
+              :key="option.key"
+            ></el-option>
+          </el-select>
+          <span v-else>{{scope.row.role}}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="账号"
+                       width="180px">
+        <template slot-scope="scope">
+          <el-input v-if="scope.row.edit" v-model="scope.row.username"></el-input>
+          <span v-else>{{scope.row.username}}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="描述"
+                       prop="des"></el-table-column>
+      <el-table-column label="操作"
+                       width="180px">
+        <template slot-scope="scope"
+                  v-if="scope.row.username !='admin'">
+          <el-button @click="handleEdit(scope.$index,scope.row)" size="mini" v-if="!scope.row.edit">编辑</el-button>
+          <el-button
+            @click="handleSave(scope.$index,scope.row)"
+            v-else
+            type="success"
+            size="mini"
+          >完成</el-button>
+          <el-button @click="handleDelete(scope.$index,scope.row)" size="mini" type="danger">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <AddAccount @update="getData" @closeDialog="closeDialog" :dialogVisible="dialogVisible" :options="options"></AddAccount>
   </div>
 </template>
 <script lang="ts">
-import { Component, Vue} from 'vue-property-decorator'
+import { Component, Vue, Provide } from "vue-property-decorator";
+import AddAccount from './AddAccount.vue'
 @Component({
-  components:{}
+  components: {AddAccount}
 })
+export default class AccountData extends Vue {
+  @Provide() tableData: any = [];
+  @Provide() dialogVisible: Boolean = false;
 
-export default class AccountData extends Vue{}
+  @Provide() options:any=[
+    {
+      key: "admin",
+      role: "管理员",
+      des: "Super Administrator. Have access to view all pages."
+    },
+    {
+      key: "editor",
+      role: "客服",
+      des: "Normal Editor. Can see all pages except permission page"
+    },
+    {
+      key: "visitor",
+      role: "游客",
+      des: "Just a visitor. Can only see the home page and the document page"
+    }
+  ]
+  created() {
+    this.getData();
+  }
+  getData() {
+    (this as any).$axios(`api/users/allUsers`).then((res: any) => {
+      res.data.datas.forEach((item) =>{
+        item.edit=false
+      })
+      this.tableData = res.data.datas;
+      console.log(this.tableData)
+    });
+  }
+  addAccount(){
+    this.dialogVisible=true
+  }
+  closeDialog(){
+    this.dialogVisible=false
+  }
+  handleEdit(index: number, row: any): void{
+    //编辑
+    row.edit=true
+  }
+  handleSave(index: number, row: any): void {
+    // 完成
+    row.edit = false;
+    (this as any).$axios
+      .post(`/api/users/editUser/${row._id}`, row)
+      .then((res: any) => {
+        this.$message({
+          message: res.data.msg,
+          type: "success"
+        });
+      });
+  }
+  handleDelete(index: number, row: any): void {
+    // 删除
+    (this as any).$axios.delete(`/api/users/deleteUser/${row._id}`).then((res:any)=>{
+      this.$message({
+          message: res.data.msg,
+          type: "success"
+        });
 
+        this.tableData.splice(index, 1);
+    })
+  }
+  selectChange(item) {
+    //选择
+    this.options.map((option:any) =>{
+      if(option.role == item.role) {
+        item.key = option.key;
+        item.des = option.des;
+      }
+    })
+  }
+}
 </script>
+<style lang="scss" scoped>
+.account-data {
+  height: 100%;
+  overflow: auto;
+  .add-box {
+    margin-bottom: 10px;
+  }
+}
+</style>
